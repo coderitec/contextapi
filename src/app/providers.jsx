8c14cd17b6917d2c8a184cfe5e0b5f5c54517e90
enemuo <francenemuo@gmail.com>
2024-06-10 18:43:39 +0100

    fix: handle localstorage not being defined

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -7,33 +7,42 @@ const SideBarContext = createContext()
 const SidebarProvider = ({children}) => {
   
   const [val, setVal] = useState(() => {
-    const savedVal = localStorage.getItem('Item num')
-    return savedVal !== null ? parseInt(savedVal, 10) : 0
+    if (typeof window !== 'undefined') {
+      const savedVal = localStorage.getItem('Item num')
+      return savedVal !== null ? parseInt(savedVal, 10) : 0
+    }
+    return 0
   });
-    const [navOpen, setNavOpen] = useState(false)
-    const [inputVal, setInputVal] = useState(1)
-    const [cartItems, setCartItems] = useState(() => {
+  const [navOpen, setNavOpen] = useState(false)
+  const [inputVal, setInputVal] = useState(1)
+  const [cartItems, setCartItems] = useState(() => {
+    if (typeof window !== 'undefined') {
       const savedCart = localStorage.getItem('Saved Cart')
       return savedCart !== null ? JSON.parse(savedCart) : []
-    });
-    const [openCart, setOpenCart] = useState(false)
+    }
+    return []
+  });
+  const [openCart, setOpenCart] = useState(false)
 
-
-    useEffect(() => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
       localStorage.setItem('Item num', val)
-    },[val])
+    }
+  }, [val])
 
-    useEffect(() => {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
       localStorage.setItem('Saved Cart', JSON.stringify(cartItems))
-    },[cartItems])
+    }
+  }, [cartItems])
 
   return (
     <div>
-        <SideBarContext.Provider value={{navOpen,setNavOpen,val,setVal,inputVal, setInputVal, openCart, setOpenCart,cartItems, setCartItems}}>
+      <SideBarContext.Provider value={{navOpen, setNavOpen, val, setVal, inputVal, setInputVal, openCart, setOpenCart, cartItems, setCartItems}}>
         {children}
-        </SideBarContext.Provider>
+      </SideBarContext.Provider>
     </div>
   )
 }
 
-export  {SideBarContext, SidebarProvider}
+export { SideBarContext, SidebarProvider }
